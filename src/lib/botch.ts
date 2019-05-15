c3b4a5084f75f86def5c6d73a8932131690c3bde
ersims <ersims88@gmail.com>
2019-05-15 22:50:13 +0200

    feat: add support for MultiError in botch function

--- src/lib/botch.ts
+++ src/lib/botch.ts
@@ -1,14 +1,66 @@
 // Imports
-import GenericError from './GenericError';
+import { MultiError, VError } from 'verror';
+import HttpError from './HttpError';
+import createHttpError from './createHttpError';
+
+// Types
+interface MaybeDetailedError {
+  data?:
+    | {
+        status?: any;
+        statusCode?: any;
+        headers?: any;
+      }
+    | any;
+  headers?: any;
+  status?: any;
+  statusCode?: any;
+  isBotched?: boolean;
+  errors?: any;
+}
 
 /**
- * Create a botch error or return the existing error if it is already a botched error
+ * Create a botch http error or return the existing error if it is already a botched http error
  *
- * @param {Error} error
- * @returns {GenericError}
+ * @param {Error} err
+ * @returns {HttpError}
  */
-function botch(error: Error): GenericError {
-  return error instanceof GenericError ? error : new GenericError(error);
+function botch(err: Error & MaybeDetailedError): HttpError {
+  // Return fast?
+  if (err instanceof HttpError) return err;
+
+  // Extract any default information
+  const data = (typeof err.data === 'object' && err.data) || VError.info(err);
+  let statusCode = err.statusCode || err.status || data.statusCode || data.status;
+  const headers = err.headers || err.headers || data.headers || data.headers;
+
+  // Check for MultiError and find the most appropriate common status code
+  // We do not aggregate headers as that could be confusing when some headers overlap between errors
+  if (err instanceof MultiError && !statusCode) {
+    const errors: (Error & MaybeDetailedError)[] = err.errors();
+
+    // Extract the most logical common status code
+    for (const error of errors) {
+      const subData = (typeof error.data === 'object' && error.data) || VError.info(error);
+      const subStatusCode = error.statusCode || error.status || subData.statusCode || subData.status || 500;
+
+      // Should we try to find a common status code (either 400 or 500)?
+      if (statusCode && subStatusCode !== statusCode) {
+        // Default to 400?
+        if (statusCode >= 400 && statusCode < 500 && subStatusCode >= 400 && subStatusCode < 500) statusCode = 400;
+        // Fallback to 500
+        else statusCode = 500;
+      } else statusCode = subStatusCode;
+
+      // Just stop if the code is 500 as that will always win
+      if (statusCode === 500) break;
+    }
+  }
+
+  // Set final statuscode
+  statusCode = statusCode || 500;
+
+  return createHttpError(statusCode, { headers, cause: err }, statusCode !== 500 ? err.message : '');
 }
 
 // Exports
--- test/__tests__/lib/botch.ts
+++ test/__tests__/lib/botch.ts
@@ -1,28 +1,122 @@
 // Dependencies
 import botch from '../../../src/lib/botch';
 import GenericError from '../../../src/lib/GenericError';
-import InternalServerError from '../../../src/lib/HttpErrors/InternalServerError';
+import { MultiError, VError } from 'verror';
+import { Forbidden, GatewayTimeout, InternalServerError, UnprocessableEntity } from '../../../src/lib/HttpErrors';
 
 // Tests
-it('should not touch a botched error', () => {
-  const error = new GenericError('My Message');
-  const botchedError = botch(error);
-  expect(error.isBotched).toBe(true);
-  expect(botchedError.isBotched).toBe(true);
-  expect(error).toBe(botchedError);
+describe('HttpError', () => {
+  it('should not touch a botched http error', () => {
+    const error = new UnprocessableEntity('My Message');
+    const botchedError = botch(error);
+    expect(error.isBotched).toBe(true);
+    expect(error).toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(422);
+    expect(botchedError.detail).toBe('My Message');
+    expect(botchedError.cause()).toBe(undefined);
+  });
+  it('should not hide the details of a botched internal server error', () => {
+    const error = new InternalServerError('My Message');
+    const botchedError = botch(error);
+    expect(error.isBotched).toBe(true);
+    expect(error).toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(500);
+    expect(botchedError.detail).toBe('My Message');
+    expect(botchedError.cause()).toBe(undefined);
+  });
 });
-it('should not touch a botched http error', () => {
-  const error = new InternalServerError('My Message');
-  const botchedError = botch(error);
-  expect(error.isBotched).toBe(true);
-  expect(botchedError.isBotched).toBe(true);
-  expect(error).toBe(botchedError);
+describe('GenericError', () => {
+  it('should create a botched http error if the error was GenericError', () => {
+    const error = new GenericError('My Message');
+    const botchedError = botch(error);
+    expect(error.isBotched).toBe(true);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(500);
+    expect(botchedError.detail).toBe(undefined);
+    expect(botchedError.cause()).toBe(error);
+    expect(botchedError instanceof InternalServerError).toBe(true);
+  });
 });
-it('should create a botched error if the error was generic', () => {
-  const error: any = new Error('My Message');
-  const botchedError = botch(error);
-  expect(error.isBotched).toBe(undefined);
-  expect(botchedError.isBotched).toBe(true);
-  expect(error).not.toBe(botchedError);
-  expect(botchedError.cause()).toBe(error);
+describe('Vanilla Error', () => {
+  it('should create a botched http error if the error was a vanilla error', () => {
+    const error = new Error('My Message');
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(500);
+    expect(botchedError.detail).toBe(undefined);
+    expect(botchedError.cause()).toBe(error);
+  });
+  it('should respect additional data', () => {
+    const error: any = new Error('My Message');
+    error.data = { status: 418 };
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(418);
+    expect(botchedError.detail).toBe('My Message');
+    expect(botchedError.cause()).toBe(error);
+  });
+});
+describe('MultiError', () => {
+  it('should create a botched http error', () => {
+    const error = new MultiError([new Error('My First Error'), new Error('My Second Error')]);
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(500);
+    expect(botchedError.detail).toBe(undefined);
+    expect(botchedError.cause()).toBe(error);
+  });
+  it('should respect the errors status code', () => {
+    const myCustomError: any = new Error('My First Error With Status 422');
+    myCustomError.data = { status: 422 };
+    const error = new MultiError([
+      myCustomError,
+      new VError({ info: { status: 422 } }, 'My Almost Real Http Error'),
+      new UnprocessableEntity('My Real Http Error'),
+    ]);
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(422);
+    expect(botchedError.detail).toBe('first of 3 errors: My First Error With Status 422');
+    expect(botchedError.cause()).toBe(error);
+  });
+  it('should default to status code 400 if there are only errors in the 400 range (and different)', () => {
+    const myCustomError: any = new Error('My First Error With Status 418');
+    myCustomError.data = { status: 418 };
+    const error = new MultiError([
+      myCustomError,
+      new VError({ info: { status: 422 } }, 'My Almost Real Http Error'),
+      new UnprocessableEntity('My Real Http Error'),
+      new Forbidden('Forbidden Error'),
+    ]);
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(400);
+    expect(botchedError.detail).toBe('first of 4 errors: My First Error With Status 418');
+    expect(botchedError.cause()).toBe(error);
+  });
+  it('should default to status code 500 if there are at least one error in the 500 range', () => {
+    const myCustomError: any = new Error('My First Error With Status 418');
+    myCustomError.data = { status: 418 };
+    const error = new MultiError([
+      myCustomError,
+      new VError({ info: { status: 422 } }, 'My Almost Real Http Error'),
+      new UnprocessableEntity('My Real Http Error'),
+      new Forbidden('Forbidden Error'),
+      new GatewayTimeout('Gateway timeout'),
+    ]);
+    const botchedError = botch(error);
+    expect(error).not.toBe(botchedError);
+    expect(botchedError.isBotched).toBe(true);
+    expect(botchedError.statusCode).toBe(500);
+    expect(botchedError.detail).toBe(undefined);
+    expect(botchedError.cause()).toBe(error);
+  });
 });
